e841234fdebd21bb608e0b2018a905fcf86a674a
imygnam <imygnam@gmail.com>
2022-08-12 01:36:28 +0900

    create

---
+++ src/main/java/com/server/pandascore/PandaScoreApplication.java
@@ -0,0 +1,13 @@
+package com.server.pandascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaScoreApplication.class, args);
+    }
+
+}
