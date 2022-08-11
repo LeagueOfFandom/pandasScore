233fcb83b68832186cd623213fab02a92eb8cffa
imygnam <imygnam@gmail.com>
2022-08-12 01:54:53 +0900

    application setting 완료. pandascore api token aws에 등록완료.

---
+++ src/main/java/com/server/pandascore/PandaScoreCrawling.java
@@ -0,0 +1,16 @@
+package com.server.pandascore;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PandaScoreCrawling implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("PandaScoreCrawling is running");
+    }
+
+}
+
