0345f981ee7a41bf27b09743ba8a61a81ef1096c
imygnam <imygnam@gmail.com>
2022-09-03 16:05:02 +0900

    [FEAT] pandascore에서 받아올 leagueListDto생성.

---
+++ src/main/java/com/server/pandascore/dto/leagueDto/sub/Series.java
@@ -0,0 +1,23 @@
+package com.server.pandascore.dto.leagueDto.sub;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class Series {
+    private LocalDateTime begin_at;
+    private String description;
+    private LocalDateTime end_at;
+    private String full_name;
+    private Long id;
+    private Long league_id;
+    private LocalDateTime modified_at;
+    private String name;
+    private String season;
+    private String slug;
+    private String tier;
+    private Long winner_id;
+    private String winner_type;
+    private Long year;
+}
---
+++ src/main/java/com/server/pandascore/dto/leagueDto/sub/VideoGame.java
@@ -0,0 +1,11 @@
+package com.server.pandascore.dto.leagueDto.sub;
+
+import lombok.Getter;
+
+@Getter
+public class VideoGame {
+    private String current_version;
+    private Long id;
+    private String name;
+    private String slug;
+}
