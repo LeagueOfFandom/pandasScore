54febf5f22e7b885378f86f24433372089ea4c78
imygnam <imygnam@gmail.com>
2022-09-22 13:38:42 +0900

    slack message 추가

--- src/main/java/com/server/pandascore/controller/PandaScoreController.java
+++ src/main/java/com/server/pandascore/controller/PandaScoreController.java
@@ -2,6 +2,7 @@ package com.server.pandascore.controller;
 
 
 import com.server.pandascore.service.pandaScore.PandaScoreCrawling;
+import com.server.pandascore.service.slack.SlackNotifyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,27 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class PandaScoreController {
 
+    private final SlackNotifyService slackNotifyService;
     private final PandaScoreCrawling pandaScoreCrawling;
 
     @PostMapping("/setAllLeague")
     public ResponseEntity<?> setAllLeague(){
+        slackNotifyService.sendMessage("setAllLeague start");
         pandaScoreCrawling.getLeagueList();
+        slackNotifyService.sendMessage("setAllLeague end");
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/setAllMatch")
     public ResponseEntity<?> setAllMatch(){
+        slackNotifyService.sendMessage("setAllMatch start");
         pandaScoreCrawling.getAllMatchList();
+        slackNotifyService.sendMessage("setAllMatch end");
         return ResponseEntity.ok().build();
     }
     @PostMapping("/setAllTeam")
     public ResponseEntity<?> setAllTeam(){
+        slackNotifyService.sendMessage("setAllTeam start");
         pandaScoreCrawling.getAllTeamList();
+        slackNotifyService.sendMessage("setAllTeam end");
         return ResponseEntity.ok().build();
     }
     @PostMapping("/setAllChampion")
     public ResponseEntity<?> setAllChampion(){
+        slackNotifyService.sendMessage("setAllChampion start");
         pandaScoreCrawling.getChampionList();
+        slackNotifyService.sendMessage("setAllChampion end");
         return ResponseEntity.ok().build();
     }
 }
