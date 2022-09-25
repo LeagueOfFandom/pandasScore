b1aade3773b3de6ee958257b19788c37f1719def
imygnam <imygnam@gmail.com>
2022-09-26 01:36:31 +0900

    사용하지 않는 entity 제거

--- src/main/java/com/server/pandascore/repository/TeamRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.server.pandascore.repository;
-
-import com.server.pandascore.entity.TeamRankingEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRankingRepository extends JpaRepository<TeamRankingEntity, Long> {
-}
